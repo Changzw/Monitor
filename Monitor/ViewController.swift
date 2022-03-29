d53f1c0dd764b3e7775f8f2e2e2d8086ea593949
czw <changzhongwei3@163.com>
2022-03-29 21:10:08 +0800

    Initial Commit

---
+++ Monitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monitor
+//
+//  Created by Fri on 2022/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
